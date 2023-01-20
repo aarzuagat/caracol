f320cf5fcd26203c7a3f44f478ccda1dc72edbf6
Arzuaga <aarzuagat@gmail.com>
2023-01-20 14:51:54 -0500

    elimina el passw de mysql

--- main/views.py
+++ main/views.py
@@ -90,6 +90,8 @@ def searchMarina():
     options.binary_location = binary_location
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-setuid-sandbox")
 
     # driver = webdriver.Firefox()
     driver = webdriver.Chrome(executable_path=driver_location, options=options)
